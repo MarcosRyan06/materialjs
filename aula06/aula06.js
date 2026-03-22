4e423da4c6189741e9d43609590517f45b9a84f4
Marcos Ryan <marcosryanjesus@gmail.com>
2026-03-22 20:57:53 -0300

    aula 06

---
+++ aula06/aula06.js
@@ -0,0 +1,22 @@
+//Incremento
+/*
+var a = 1
+a ++ mesma coisa que (a = a +1)
+a vale 2
+a ++
+a vale 3
+*/
+//Decremento
+/*
+var b = 10
+b --
+b vale 9
+b --
+b vale 8
+ */
+
+var numero = Number(prompt("Digite entre 1 a 10 "));
+while(numero < 10){
+    document.write("Olá juvenil <br>");
+    numero ++;
+}
