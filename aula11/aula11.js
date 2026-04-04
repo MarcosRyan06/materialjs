713bf4ab76fee56a82a28fddd71d40264a890590
Marcos Ryan <marcosryanjesus@gmail.com>
2026-04-04 11:04:56 -0300

    Aula 11 - fim

---
+++ aula11/aula11.js
@@ -0,0 +1,10 @@
+// var nome = "Ryan";
+//esse length ai serve para contar e mostrar na tela o numerode caracteres
+// document.write(nome.length);
+
+const nomes = ["Marcos", "Ryan", "Naninha", "Koxa", "Davids", "Abelinha"];
+
+//Quando se trata de vetores o length serve para contar a posição do elementos 
+for(let i = 0; i < nomes.length; i++){
+    document.write(nomes[i] + "<br>");
+};
