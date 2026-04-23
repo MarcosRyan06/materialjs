11fa49d33942200e195e3dea01a5f6e9f2c8a8cb
Marcos Ryan <marcosryanjesus@gmail.com>
2026-04-22 22:58:47 -0300

    aula 1 - console.log

---
+++ cursoJS/cursoIntermediario/aula1/aula1.js
@@ -0,0 +1,7 @@
+var nome = "Ryan";
+var idade = 20;
+
+console.log(`${nome} tem ${idade} anos`);
+
+//O console.log aparece apenas para os devs e não usuarios
+//Ele serve para testes rapidos e depuração de codigo
