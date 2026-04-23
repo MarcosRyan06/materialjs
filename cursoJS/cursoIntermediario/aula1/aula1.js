a6a747828eb12583ddf9f5aeead3b7607cfab385
Marcos Ryan <marcosryanjesus@gmail.com>
2026-04-22 23:22:19 -0300

    console.log

--- cursoJS/cursoIntermediario/aula1/aula1.js
+++ cursoJS/cursoIntermediario/aula1/aula1.js
@@ -5,3 +5,9 @@ console.log(`${nome} tem ${idade} anos`);
 
 //O console.log aparece apenas para os devs e não usuarios
 //Ele serve para testes rapidos e depuração de codigo
+
+var tabuada = 5;
+for(let i = 1; i <= 10; i++){
+    let total = tabuada * i;
+    console.log(`${tabuada} x ${i} = ${total}`);
+}
